f1671accdce9453b6c1131c9a8c6fa7362fd7284
Nischal <nischalshrestha800@gmail.com>
2024-06-20 22:20:21 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Routes } from 'react-router-dom';
+import Blogs from './components/Blogs';
+import CreateBlog from './components/CreateBlog';
+
+function App() {
+
+  return (
+    <>
+      <div className="bg-dark text-center py-2 shadow-lg">
+        <h1 className='text-white'>React & Laravel Blog App</h1>
+      </div>
+
+      <Routes>
+        <Route path='/' element={<Blogs />} />
+        <Route path='/create' element={<CreateBlog />} />
+      </Routes>
+
+
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/CreateBlog.jsx
@@ -0,0 +1,85 @@
+import React, { useState } from 'react'
+import { useForm } from 'react-hook-form'
+const CreateBlog = () => {
+    const [html, setHtml] = useState('')
+
+    function onChange(e) {
+        setHtml(e.target.value);
+    }
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+    } = useForm()
+
+    const formSubmit = async (data) => {
+
+        const res = await fetch("http://localhost:8000/api/blogs", {
+            method: "POST",
+            headers: {
+                'Content-type': 'appliacation/json'
+            },
+            body: JSON.stringify(data)
+        });
+        // console.log(data);
+    }
+    return (
+        <div className='container mb-5'>
+            <div className="d-flex justify-content-between pt-5 mb-4 " >
+                <h4>Create Blog</h4>
+                <a href="/ " className='btn btn-dark'>Back</a>
+            </div>
+            <div className="card border-0 shadow-lg">
+                <form onSubmit={handleSubmit(formSubmit)}>
+                    <div className="card-body">
+                        <div className="mb-3">
+                            <label className='form-label'><h1>Title:</h1></label>
+                            <input
+                                {...register('title', { required: true })}
+                                type="text"
+                                className={`form-control ${errors.title && 'is-invalid'}`}
+                                placeholder="Title" />
+                            {errors.title && <p className='invalid-feedback'>Title field is required</p>}
+                        </div>
+
+                        <div className="mb-3">
+                            <label className='form-label'><h1>Short Description:</h1></label>
+                            <textarea
+                                {...register('shortDesc')}
+                                className='form-control' cols="30" rows="5"></textarea>
+                        </div>
+
+                        <div className="mb-3">
+                            <label className='form-label'><h1>Description:</h1></label>
+                            <textarea
+                                {...register('description')}
+                                className='form-control' cols="30" rows="10"></textarea>
+
+                        </div>
+                        <div className="mb-3">
+                            <label className='form-label'><h1>Image</h1></label><br />
+                            <input type="file" />
+                        </div>
+
+                        <div className="mb-3">
+                            <label className='form-label'><h1>Author Name:</h1></label>
+                            <input
+                                {...register('author', { required: true })}
+
+                                type="text"
+                                className={`form-control ${errors.author && 'is-invalid'}`}
+                                placeholder='Author' />
+                            {errors.author && <p className='invalid-feedback'>Author field is required</p>}
+
+                        </div>
+                        <button className='btn btn-dark'>Create</button>
+                    </div>
+                </form>
+            </div>
+
+        </div>
+    )
+}
+
+export default CreateBlog
